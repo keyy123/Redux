5442bd6509472c5efa891063a4c754db01600a73
keyy123 <parktwins1@gmail.com>
2021-12-17 17:12:58 -0500

    tinkering

--- reduxing/react-redux/src/App.js
+++ reduxing/react-redux/src/App.js
@@ -1,5 +1,5 @@
-import "./output.css"
-import "./App.css"
+// import "./output.css"
+// import "./App.css"
 import Cake from './components/Cake'
 import store  from './redux/store.js'
 import { Provider } from 'react-redux';
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 function App() {
   return (
    <Provider store={store}>
-    <div className="bg-red-600">
+    <div className="bg-rose-600">
       <Cake/>
     </div>
     </Provider>
